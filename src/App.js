41dcb04d00bfbde577a8fc087a3631b78cc90431
Zobair <zobairq@outlook.com>
2020-04-30 17:14:12 +0200

    Navigation and routing is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage/Homepage';
+import React from "react";
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
 
 function App() {
   return (
     <div>
-    <Homepage/>
+      <Switch>
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+        <Route path='/shop/hats'>
+
+        </Route>
+        <Route path='/shop/jackets'>
+      
+          </Route>
+          <Route path='shop/sneakers'>
+          
+          </Route>
+          <Route path='shop/womens'>
+          
+          </Route>
+          <Route path='shop/mens'>
+          
+          </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./menu.item.styles.scss";
+import {withRouter} from 'react-router-dom';
 export class MenuItem extends Component {
   render() {
     return (
-      <div className={`${this.props.size} menu-item`}>
+      <div className={`${this.props.size} menu-item`} onClick={() => 
+      this.props.history.push(`${this.props.match.url}${this.props.linkUrl}`)}>
         <div className = 'background-image' style={{
-          backgroundImage: `url(${this.props.imageurl})`,
+          backgroundImage: `url(${this.props.imageUrl})`,
         }}/>
           <div className="content">
             <h1 className="title">{this.props.title}</h1>
@@ -16,4 +18,4 @@ export class MenuItem extends Component {
   }
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
